d03ca7461622cae22cb56f534bee7f7ff85dc63e
alanalbertoriva <alan.alberto.riva@gmail.com>
2025-08-22 12:30:38 -0300

    Modificación de rutas data.js

--- js/data.js
+++ js/data.js
@@ -1,9 +1,9 @@
 let url = '';
 
-if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
-    url = "./js/data.json";
-} else {
+if (window.location.pathname.includes('adopcion.html') || window.location.pathname.includes('adopciones.html') || window.location.pathname.includes('altahuella.html') || window.location.pathname.includes('animales.html') || window.location.pathname.includes('busqueda.html') || window.location.pathname.includes('cambiohuella.html')) {
     url = "../js/data.json";
+} else {
+    url = "./js/data.json";
 }
 
 async function fetchAnimals() {
